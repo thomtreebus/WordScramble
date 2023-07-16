cb20da9f74b73df72c69c5e841bf3c8385a230bc
Thom Treebus <thomtreebus@gmail.com>
2023-07-16 17:50:24 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Thom Treebus on 16/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
